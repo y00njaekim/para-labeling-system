508baf91848e03eff7bfdb48e0e00447ad788a0d
y00njae <rladbswo12@gist.ac.kr>
2024-03-11 22:55:55 +0900

    다크 모드 설정 해제

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-20 bg-gradient-to-r from-gray-50 to-gray-100 dark:from-zinc-900 dark:to-zinc-800 ">
+    <main className="flex min-h-screen flex-col items-center p-20 bg-gradient-to-r from-gray-50 to-gray-100 ">
       <section className="w-full py-12 flex items-center justify-center">
         <div className="container px-4 md:px-6">
           <div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2 md:gap-8">
-            <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+            <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg justify-between border border-gray-300">
               <div>
                 <h3 className="text-2xl font-bold text-center">Study 1</h3>
                 <div className="w-48 h-48 mx-auto py-5">
@@ -38,7 +38,7 @@ export default function Home() {
                 </Link>
               </div>
             </div>
-            <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+            <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg justify-between border border-gray-300">
               <div>
                 <h3 className="text-2xl font-bold text-center">Study 2</h3>
                 <div className="w-48 h-48 mx-auto py-5">
